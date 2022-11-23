aadb92ca2e86eea8c808107b98144c1a88e42b82
nefure <1939295014@qq.com>
2022-11-23 11:50:07 +0800

    init

---
+++ src/main/java/org/nefure/goodshops/GoodShopsApplication.java
@@ -0,0 +1,13 @@
+package org.nefure.goodshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodShopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodShopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nefure/goodshops/GoodShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nefure.goodshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodShopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
